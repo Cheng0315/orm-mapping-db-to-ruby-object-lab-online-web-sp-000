4fdab0476595d0fb431bf26f5baa1e3b71d0aeb6
Cheng Vue <cvu3@ucsc.edu>
2018-12-25 22:44:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,8 +8,12 @@ class Student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+
+    DB[:conn].execute(sql).map {|d| self.new_from_db(d)}
   end
 
   def self.find_by_name(name)
