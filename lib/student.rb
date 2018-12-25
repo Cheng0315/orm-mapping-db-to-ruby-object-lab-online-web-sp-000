45b4dab8220628b62457531bb0a9d475182612ce
Cheng Vue <cvu3@ucsc.edu>
2018-12-25 22:54:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,8 +19,14 @@ class Student
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE students.name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map {|d| self.new_from_db(d)}
   end
 
   def save
